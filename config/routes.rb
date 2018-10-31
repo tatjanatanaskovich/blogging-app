aaaa2da9be4cbfaa04e88eece39029a06924982e
Tanja <tatjanatanaskovich@gmail.com>
2018-10-31 12:33:47 +0100

    Implemented blog status change and added data validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :blogs
-  root 'blogs#index'
+
+  resources :blogs do
+    member do
+      get :change_blog_status
+    end
+  end
+
+  root to: 'blogs#index'
 end
