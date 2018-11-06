3c9e906bfb369150144e73d1dfa29f0f8e674126
Tanja <tatjanatanaskovich@gmail.com>
2018-11-06 09:14:15 +0100

    Refactored blogs controller to be more readable

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,59 +1,44 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :change_blog_status]
 
-  # GET /blogs
-  # GET /blogs.json
   def index
     @blogs = Blog.all
   end
 
-  # GET /blogs/1
-  # GET /blogs/1.json
   def show
   end
 
-  # GET /blogs/new
   def new
     @blog = Blog.new
   end
 
-  # GET /blogs/1/edit
   def edit
   end
 
-  # POST /blogs
-  # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
     @blog.user = current_user
-    respond_to do |format|
-      if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-      else
-        format.html { render :new }
-      end
+    if @blog.save
+      notice = 'Blog was successfully created.'
+      redirect_to @blog, notice: notice
+    else
+      render :new 
     end
   end
 
-  # PATCH/PUT /blogs/1
-  # PATCH/PUT /blogs/1.json
   def update
-    respond_to do |format|
-      if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-      else
-        format.html { render :edit }
-      end
+    if @blog.update(blog_params)
+      notice = 'Blog was successfully updated.'
+      redirect_to @blog, notice: notice
+    else
+      render :edit 
     end
   end
 
-  # DELETE /blogs/1
-  # DELETE /blogs/1.json
   def destroy
     @blog.destroy
-    respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
-    end
+    notice = 'Blog was successfully destroyed.'
+    redirect_to blogs_url, notice: notice
   end
 
   def change_blog_status
@@ -62,16 +47,16 @@ class BlogsController < ApplicationController
       elsif @blog.published?
         @blog.draft!
     end
-    redirect_to blogs_url, notice: 'Post status has been updated.'
+    notice = 'Post status has been updated.'
+    redirect_to blogs_url, notice: notice
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_blog
       @blog = Blog.friendly.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
       params.require(:blog).permit(:title, :body, :status, :user_id)
     end
